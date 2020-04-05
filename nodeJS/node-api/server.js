293d02bc39b84251315ad2aa1c5f03f0ad6fd1a1
Ricardo Marcon <ricardo.f.marcon@gmail.com>
2020-04-05 00:10:00 -0300

    Finished nodeJS course (node-api)

---
+++ nodeJS/node-api/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const requireDir = require('require-dir');
+const mongoose = require('mongoose');
+
+// Iniciando o app
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// Iniciando o DB
+mongoose.connect('mongodb://localhost:27017/nodeapi', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+requireDir('./src/models');
+
+app.use('/api', require('./src/routes'));
+
+app.listen(3333);
